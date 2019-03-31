cb15b66c14124e6fb66ef92198761dbddbac5690
Atang Sutisna <atang.sutisna.87@gmail.com>
2019-03-31 11:19:50 +0700

    alter table city

---
+++ db/migrations/20190331041758_alter_city_table.php
@@ -0,0 +1,44 @@
+<?php
+
+
+use Phinx\Migration\AbstractMigration;
+
+class AlterCityTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    addCustomColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Any other destructive changes will result in an error when trying to
+     * rollback the migration.
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function up()
+    {
+        $table = $this->table('cities');
+        $table->renameColumn('modification_time', 'creation_time');
+    }
+
+    public function down()
+    {
+        $table = $this->table('cities');
+        $table->renameColumn('creation_time', 'modification_time');
+    }
+}
