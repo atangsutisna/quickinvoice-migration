5de6f21e1cc1b4fb901d62a7e4ad1ac462820c62
Atang Sutisna <atang.sutisna.87@gmail.com>
2019-04-15 06:06:55 +0700

    allowed to null value

--- db/migrations/20190414224514_add_column_shipment_status_to_table_orders.php
+++ db/migrations/20190414224514_add_column_shipment_status_to_table_orders.php
@@ -15,10 +15,11 @@ class AddColumnShipmentStatusToTableOrders extends AbstractMigration
             [
                 'values' => ['draft', 'shipped'], 
                 'default' => 'draft',
-                'after' => 'payment_time'
+                'after' => 'payment_time',
+                'null' => true
             ])
-            ->addColumn('shipment_time', 'datetime', ['after' => 'shipment_status'])
-            ->addColumn('tracking_number', 'string', ['limit' => 250, 'after' => 'shipment_time'])
+            ->addColumn('shipment_time', 'datetime', ['after' => 'shipment_status', 'null' => true])
+            ->addColumn('tracking_number', 'string', ['limit' => 250, 'after' => 'shipment_time', 'null' => true])
             ->save();
     }
 }
