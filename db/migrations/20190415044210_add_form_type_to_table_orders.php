2b2364eba0d486e19ee9ce890550cfe9e73f220b
Atang Sutisna <atang@bippo.co.id>
2019-04-15 11:44:04 +0700

    add form type to order table

---
+++ db/migrations/20190415044210_add_form_type_to_table_orders.php
@@ -0,0 +1,17 @@
+<?php
+
+
+use Phinx\Migration\AbstractMigration;
+
+class AddFormTypeToTableOrders extends AbstractMigration
+{
+    /**
+     * Up Method.
+     */
+    public function up()
+    {
+        $table = $this->table('orders');
+        $table->addColumn('form_type', 'enum', ['values' => ['simple', 'advance'], 'default' => 'simple'])
+              ->save();
+    }
+}
