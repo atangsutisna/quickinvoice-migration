eed9330a09bf7aca6bdd9f79298968844786686b
Atang Sutisna <atang.sutisna.87@gmail.com>
2019-05-29 07:42:15 +0700

    add customer_id

---
+++ db/migrations/20190529004005_add_customer_id_to_orders.php
@@ -0,0 +1,17 @@
+<?php
+
+
+use Phinx\Migration\AbstractMigration;
+
+class AddCustomerIdToOrders extends AbstractMigration
+{
+    /**
+     * Up Method.
+     */
+    public function up()
+    {
+        $table = $this->table('customer');
+        $table->addColumn('customer_id', 'integer', ['after' => 'form_type'])
+              ->update();
+    }
+}
